5be2d53fb41fe848f04531aa6f57ea2d863b6fc7
tsuyan <rafaeldesenhos99@gmail.com>
2020-08-03 13:33:45 +0000

    ✨ All tests passed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +38,14 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const newFood = {
+        id: foods.length + 1,
+        ...food,
+        available: true,
+      };
+
+      const response = await api.post('/foods', newFood);
+      setFoods(state => [response.data, ...state]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +54,23 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const updatedFood = {
+      ...editingFood,
+      ...food,
+    };
+
+    const response = await api.put(`/foods/${editingFood.id}`, updatedFood);
+
+    const updatedState = foods.filter(
+      findFood => findFood.id !== updatedFood.id,
+    );
+
+    setFoods([response.data, ...updatedState]);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    setFoods(state => state.filter(food => food.id !== id));
   }
 
   function toggleModal(): void {
@@ -62,7 +82,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    setEditModalOpen(true);
   }
 
   return (
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    'https://3333-e5054210-7259-4789-bac8-f9d350b63546.ws-us02.gitpod.io/',
 });
 
 export default api;
