71397e2e8b3d13822ee4e131bd399c03537b1360
tsuyan <53716129+tsuyusk@users.noreply.github.com>
2020-08-03 12:29:05 -0300

    *Fixing api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    'https://3333-e5054210-7259-4789-bac8-f9d350b63546.ws-us02.gitpod.io/',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
